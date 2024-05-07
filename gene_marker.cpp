81ffd07220d9bd894af545e86a32ad9e2e9b4472
Guoyi Zhang <kuoi@bioarchlinux.org>
2024-05-07 12:39:13 +0800

    polish: same style

--- gene_marker.cpp
+++ gene_marker.cpp
@@ -15,7 +15,7 @@
 bool checkArguments(int argc, char* argv[]) {
 	if (argc != 3) {
 		std::cerr << "Usage: " << argv[0]
-			  << " input_file output_file\n";
+			  << " <input_file> <output_file>\n";
 		return false;
 	}
 	return true;
